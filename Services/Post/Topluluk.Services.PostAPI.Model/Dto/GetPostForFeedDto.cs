f4f278a6627f085a59f57a56c1df7f7ca1af8e0c
ismail ertuğlu <ismailertgl@gmail.com>
2023-03-25 23:10:02 +0300

    Get Posts for feed screen UserId return fix.

--- Services/Post/Topluluk.Services.PostAPI.Model/Dto/GetPostForFeedDto.cs
+++ Services/Post/Topluluk.Services.PostAPI.Model/Dto/GetPostForFeedDto.cs
@@ -3,7 +3,7 @@
 public class GetPostForFeedDto
 {
     public string Id { get; set; }
-
+    public string UserId { get; set; }
     public string FirstName { get; set; }
     public string LastName { get; set; }
     public string? ProfileImage { get; set; }
