c50ab6b8e498eff692dc218826f6356bca08abdf
ismail ertuğlu <ismailertgl@gmail.com>
2023-03-25 23:18:30 +0300

    .

--- Services/Post/Topluluk.Services.PostAPI.Model/Entity/Post.cs
+++ Services/Post/Topluluk.Services.PostAPI.Model/Entity/Post.cs
@@ -6,7 +6,7 @@ namespace Topluluk.Services.PostAPI.Model.Entity
 {
 	public class Post : AbstractEntity
 	{
-		public string UserId { get; set; }
+		public string UserId { get; set; } 
 		public string FirstName { get; set; }
 		public string LastName { get; set; }
 		public string? ProfileImage { get; set; }
