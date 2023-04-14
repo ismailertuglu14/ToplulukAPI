4ffcf756e51da66ab4ba3059bdcb9d73416c86f8
ismail <hazretismaiiiiil14@outlook.com>
2023-04-14 23:16:50 +0300

    .

---
+++ Services/Authentication/Topluluk.Services.AuthenticationAPI.Model/Dto/ResetPasswordDto.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace Topluluk.Services.AuthenticationAPI.Model.Dto
+{
+    public class ResetPasswordDto
+    {
+        public string NewPassword { get; set; }
+        public string NewPasswordAgain { get; set; }
+    }
+}
+
