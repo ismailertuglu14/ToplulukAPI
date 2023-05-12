21d431704bde10ce24cbd2af653551064e57fd68
İsmail Ertuğlu <ismail.ertuglu@quanticst.com>
2023-05-12 22:46:13 +0300

    .

--- Services/Community/Topluluk.Services.CommunityAPI.Services/Interface/ICommunityService.cs
+++ Services/Community/Topluluk.Services.CommunityAPI.Services/Interface/ICommunityService.cs
@@ -14,7 +14,7 @@ namespace Topluluk.Services.CommunityAPI.Services.Interface
 		Task<Response<List<CommunityGetPreviewDto>>> GetCommunitySuggestions(string userId, HttpRequest request, int skip = 0, int take = 5 );
 	
 		// Community Detail Page
-		Task<Response<CommunityGetByIdDto>> GetCommunityById(string userId, string id);
+		Task<Response<CommunityGetByIdDto>> GetCommunityById(string userId,string token, string id);
 		Task<Response<string>> Join(string userId, string token, CommunityJoinDto communityInfo);
 		Task<Response<string>> Create(string userId, string token, CommunityCreateDto communityInfo);
 		Task<Response<NoContent>> Leave(string userId, string token, string communityId);
