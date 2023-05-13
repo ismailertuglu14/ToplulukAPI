2f2cd6e897f77ed3872e72acc76e4fa6de1e26e9
İsmail Ertuğlu <ismail.ertuglu@quanticst.com>
2023-05-13 21:07:18 +0300

    Mongo Generic Repository Code Refactore.

--- Shared/DBHelper/Repository/Mongo/MongoGenericRepository.cs
+++ Shared/DBHelper/Repository/Mongo/MongoGenericRepository.cs
@@ -10,25 +10,35 @@ namespace DBHelper.Repository.Mongo
     public class MongoGenericRepository<T> : IGenericRepository<T> where T : AbstractEntity
     {
         private readonly IConnectionFactory _connectionFactory;
+        private readonly IMongoCollection<T> _collection;
+
 
         public MongoGenericRepository(IConnectionFactory connectionFactory)
         {
             _connectionFactory = connectionFactory;
+            _collection = GetCollection();
         }
 
-        private IMongoDatabase GetConnection() => (MongoDB.Driver.IMongoDatabase)_connectionFactory.GetConnection;
+        private IMongoDatabase GetConnection()
+        {
+            return (MongoDB.Driver.IMongoDatabase)_connectionFactory.GetConnection;
+        }
 
-        private string GetCollectionName() => string.Format("{0}Collection", typeof(T).Name);
-
+        private IMongoCollection<T> GetCollection()
+        {
+            var database = GetConnection();
+            var collectionName = typeof(T).Name + "Collection";
+            return database.GetCollection<T>(collectionName);
+        }
+        
         //todo: Need performence improvements
         public DatabaseResponse BulkUpdate(List<T> entityList)
         {
-            var database = GetConnection();
-            var collectionName = GetCollectionName();
+
 
             foreach (T entity in entityList)
             {
-                var data = database.GetCollection<T>(collectionName).ReplaceOne(x => x.Id == entity.Id, entity);
+                var data = _collection.ReplaceOne(x => x.Id == entity.Id, entity);
             }
             var dbResponse = new DatabaseResponse();
             dbResponse.IsSuccess = true;
@@ -49,30 +59,24 @@ namespace DBHelper.Repository.Mongo
 
         public void Delete(string id)
         {
-            var database = GetConnection();
-            var collectionName = GetCollectionName();
 
-            database.GetCollection<T>(collectionName).DeleteOne(x=>x.Id == id);
+            _collection.DeleteOne(x=>x.Id == id);
         }
 
         public void DeleteByExpression(Expression<Func<T, bool>> predicate)
         {
-            var database = GetConnection();
-            var collectionName = GetCollectionName();
 
             var filter = Builders<T>.Filter.Where(predicate);
             var update = Builders<T>.Update.Set(x => x.IsDeleted, true);
-            database.GetCollection<T>(collectionName).UpdateMany(filter, update);
+            _collection.UpdateMany(filter, update);
         }
 
         public int Delete(string[] id)
         {
-            var database = GetConnection();
-            var collectionName = GetCollectionName();
-
+          
             foreach (string userId in id)
             {
-                database.GetCollection<T>(collectionName).DeleteOne(x => x.Id == userId);
+                _collection.DeleteOne(x => x.Id == userId);
             }
             return 1;
         }
@@ -90,12 +94,9 @@ namespace DBHelper.Repository.Mongo
         // Find by entity's id and update with new entity
         public DatabaseResponse DeleteById(T entity)
         {
-            var database = GetConnection();
-            var collectionName = GetCollectionName();
-
             var filter = Builders<T>.Filter.Eq("_id", ObjectId.Parse(entity.Id));
             var update = Builders<T>.Update.Set(x => x.IsDeleted, true);
-            var deleteResponse = database.GetCollection<T>(collectionName).UpdateOne(filter,update);
+            var deleteResponse = _collection.UpdateOne(filter,update);
             DatabaseResponse response = new();
             response.IsSuccess = true;
             response.Data = deleteResponse.ToString();
@@ -106,12 +107,9 @@ namespace DBHelper.Repository.Mongo
         // Find by id and update IsDeleted to true
         public DatabaseResponse DeleteById(string id)
         {
-            var database = GetConnection();
-            var collectionName = GetCollectionName();
-
             var filter = Builders<T>.Filter.Eq("_id", ObjectId.Parse(id));
             var update = Builders<T>.Update.Set(x => x.IsDeleted, true);
-            var deleteResponse = database.GetCollection<T>(collectionName).UpdateOne(filter,update);
+            var deleteResponse = _collection.UpdateOne(filter,update);
             DatabaseResponse response = new();
             response.IsSuccess = true;
             response.Data = deleteResponse.ToString();
@@ -120,10 +118,7 @@ namespace DBHelper.Repository.Mongo
 
         public void DeleteCompletely(string id)
         {
-            var database = GetConnection();
-            var collectionName = GetCollectionName();
-
-            database.GetCollection<T>(collectionName).DeleteOne(x => x.Id == id);
+            _collection.DeleteOne(x => x.Id == id);
         }
 
         public void ExecuteScript(string script)
@@ -148,13 +143,10 @@ namespace DBHelper.Repository.Mongo
 
         public async Task<DatabaseResponse> GetAllAsync(int pageSize, int pageNumber, Expression<Func<T, bool>> predicate = null)
         {
-            var database = GetConnection();
-            var collectionName = GetCollectionName();
-
             var defaultFilter = Builders<T>.Filter.Eq(x => x.IsDeleted, false);
             var finalFilter = Builders<T>.Filter.And(defaultFilter, predicate);
 
-            var result = await database.GetCollection<T>(collectionName).Find(finalFilter).Skip(pageNumber * pageSize).Limit(pageSize).ToListAsync();
+            var result = await _collection.Find(finalFilter).Skip(pageNumber * pageSize).Limit(pageSize).ToListAsync();
 
             DatabaseResponse response = new();
             response.Data = result;
@@ -165,12 +157,9 @@ namespace DBHelper.Repository.Mongo
 
         public DatabaseResponse GetAllWithDeleted()
         {
-            var database = GetConnection();
-            var collectionName = GetCollectionName();
-
             var filter = Builders<T>.Filter.Empty;
 
-            var result = database.GetCollection<T>(collectionName).Find(filter).ToList();
+            var result = _collection.Find(filter).ToList();
 
             DatabaseResponse response = new();
             response.Data = result;
@@ -181,12 +170,9 @@ namespace DBHelper.Repository.Mongo
 
         public async Task<DatabaseResponse> GetAllWithDeletedAsync()
         {
-            var database = GetConnection();
-            var collectionName = GetCollectionName();
-
             var filter = Builders<T>.Filter.Empty;
 
-            var result = await database.GetCollection<T>(collectionName).Find(filter).ToListAsync();
+            var result = await _collection.Find(filter).ToListAsync();
 
             DatabaseResponse response = new();
             response.Data = result;
@@ -197,19 +183,13 @@ namespace DBHelper.Repository.Mongo
 
         public T GetByExpression(Expression<Func<T, bool>>? predicate = null)
         {
-            var database = GetConnection();
-            var collectionName = GetCollectionName();
-
-            T entity = database.GetCollection<T>(collectionName).Find(predicate).FirstOrDefault();
+            T entity = _collection.Find(predicate).FirstOrDefault();
             return entity;
         }
 
         public async Task<T> GetByExpressionAsync(Expression<Func<T, bool>>? predicate = null)
         {
-            var database = GetConnection();
-            var collectionName = GetCollectionName();
-
-            T entity = await database.GetCollection<T>(collectionName).Find(predicate).FirstOrDefaultAsync();
+            T entity = await _collection.Find(predicate).FirstOrDefaultAsync();
 
             return entity;
         }
@@ -220,10 +200,7 @@ namespace DBHelper.Repository.Mongo
         {
             var defaultFilter = Builders<T>.Filter.Eq(x => x.IsDeleted, false);
 
-            var database = GetConnection();
-            var collectionName = GetCollectionName();
-
-            var data = database.GetCollection<T>(collectionName).Find(x => x.Id == id).FirstOrDefault();
+            var data = _collection.Find(x => x.Id == id).FirstOrDefault();
 
             DatabaseResponse response = new();
             response.Data = data;
@@ -239,10 +216,8 @@ namespace DBHelper.Repository.Mongo
 
         public Task<bool> AnyAsync(Expression<Func<T, bool>> predicate)
         {
-            var database = GetConnection();
-            var collectionName = GetCollectionName();
-
-            return database.GetCollection<T>(collectionName).Find(predicate).AnyAsync();
+       
+            return _collection.Find(predicate).AnyAsync();
         }
 
         public DatabaseResponse GetByIdWithDeleted(string id)
@@ -264,36 +239,27 @@ namespace DBHelper.Repository.Mongo
         {
             var defaultFilter = Builders<T>.Filter.Eq(x => x.IsDeleted, false);
             var finalFilter = Builders<T>.Filter.And(defaultFilter, predicate);
-
-            var database = GetConnection();
-            var collectionName = GetCollectionName();
-
-            return database!.GetCollection<T>(collectionName).Find(finalFilter).FirstOrDefault();
+            
+            return _collection.Find(finalFilter).FirstOrDefault();
         }
 
         public async Task<T> GetFirstAsync(Expression<Func<T, bool>>? predicate = null)
         {
             var defaultFilter = Builders<T>.Filter.Eq(x => x.IsDeleted, false);
             var finalFilter = Builders<T>.Filter.And(defaultFilter, predicate);
-
-            var database = GetConnection();
-            var collectionName = GetCollectionName();
-
-            return await database!.GetCollection<T>(collectionName).Find(finalFilter).FirstOrDefaultAsync();
+            
+            return await _collection.Find(finalFilter).FirstOrDefaultAsync();
         }
 
         public T GetFirstWithDeleted(Expression<Func<T, bool>>? predicate = null)
         {
-            var database = GetConnection();
-            var collectionName = string.Format("{0}Collection", typeof(T).Name);
-            return database.GetCollection<T>(collectionName).Find(predicate).FirstOrDefault();
+            return _collection.Find(predicate).FirstOrDefault();
         }
 
         public async Task<T> GetFirstWithDeletedAsync(Expression<Func<T, bool>>? predicate = null)
         {
-            var database = GetConnection();
-            var collectionName = GetCollectionName();
-            return await database.GetCollection<T>(collectionName).Find(predicate).FirstOrDefaultAsync();
+          
+            return await _collection.Find(predicate).FirstOrDefaultAsync();
 
         }
 
@@ -307,10 +273,7 @@ namespace DBHelper.Repository.Mongo
             var defaultFilter = Builders<T>.Filter.Eq(x => x.IsDeleted, false);
             var finalFilter = Builders<T>.Filter.And(defaultFilter, predicate);
 
-            var database = GetConnection();
-            var collectionName = GetCollectionName();
-
-            var data = database.GetCollection<T>(collectionName).Find(finalFilter).ToList();
+            var data = _collection.Find(finalFilter).ToList();
 
             return data;
         }
@@ -320,10 +283,7 @@ namespace DBHelper.Repository.Mongo
             var defaultFilter = Builders<T>.Filter.Eq(x => x.IsDeleted, false);
             var finalFilter = Builders<T>.Filter.And(defaultFilter, predicate);
 
-            var database = GetConnection();
-            var collectionName = GetCollectionName();
-
-            var data = database.GetCollection<T>(collectionName).Find(finalFilter).ToList();
+            var data = _collection.Find(finalFilter).ToList();
 
             return await Task.FromResult(data);
         }
@@ -332,11 +292,9 @@ namespace DBHelper.Repository.Mongo
         {
             var defaultFilter = Builders<T>.Filter.Eq(x => x.IsDeleted, false);
             var finalFilter = Builders<T>.Filter.And(defaultFilter, predicate);
+            
 
-            var database = GetConnection();
-            var collectionName = GetCollectionName();
-
-            var data = database.GetCollection<T>(collectionName).Find(finalFilter).Skip(skip * take).Limit(take).ToList();
+            var data = _collection.Find(finalFilter).Skip(skip * take).Limit(take).ToList();
 
             return data;
             
@@ -344,11 +302,7 @@ namespace DBHelper.Repository.Mongo
 
         public List<T>  GetListByExpressionWithDeleted(Expression<Func<T, bool>>? predicate = null)
         {
-            var database = GetConnection();
-            var collectionName = GetCollectionName();
-
-            var data = database.GetCollection<T>(collectionName).Find(predicate).ToList();
-            
+            var data = _collection.Find(predicate).ToList();
             return data;
         }
 
@@ -359,12 +313,9 @@ namespace DBHelper.Repository.Mongo
 
         public DatabaseResponse Insert(T entity)
         {
-            var database = GetConnection();
-            var collectionName = string.Format("{0}Collection", typeof(T).Name);
-
             entity.CreatedAt = DateTime.Now;
 
-            database.GetCollection<T>(collectionName).InsertOne(entity);
+            _collection.InsertOne(entity);
 
             var dbResponse = new DatabaseResponse();
             dbResponse.IsSuccess = true;
@@ -375,12 +326,9 @@ namespace DBHelper.Repository.Mongo
 
         public async Task<DatabaseResponse> InsertAsync(T entity)
         {
-            var database = GetConnection();
-            var collectionName = string.Format("{0}Collection", typeof(T).Name);
-
             entity.CreatedAt = DateTime.Now;
 
-            await database.GetCollection<T>(collectionName).InsertOneAsync(entity);
+            await _collection.InsertOneAsync(entity);
 
             var dbResponse = new DatabaseResponse();
             dbResponse.IsSuccess = true;
@@ -397,11 +345,7 @@ namespace DBHelper.Repository.Mongo
         public DatabaseResponse Update(T entity)
         {
             entity.UpdatedAt = DateTime.Now;
-
-            var database = GetConnection();
-            var collectionName = string.Format("{0}Collection", typeof(T).Name);
-
-            var data = database.GetCollection<T>(collectionName).ReplaceOne(x => x.Id == entity.Id, entity);
+            var data = _collection.ReplaceOne(x => x.Id == entity.Id, entity);
             var dbResponse = new DatabaseResponse();
             dbResponse.IsSuccess = true;
             dbResponse.Data = data;
@@ -412,11 +356,10 @@ namespace DBHelper.Repository.Mongo
         public async Task<DatabaseResponse> GetListByIdAsync(List<string> ids)
         {
             DatabaseResponse response = new();
-            var database = GetConnection();
-            var collectionName = GetCollectionName();
+         
             var filter = Builders<T>.Filter.In(x => x.Id, ids);
          
-            response.Data = await database.GetCollection<T>(collectionName).Find(filter).ToListAsync();
+            response.Data = await _collection.Find(filter).ToListAsync();
 
             response.IsSuccess = true;
             return await Task.FromResult(response);
@@ -424,10 +367,7 @@ namespace DBHelper.Repository.Mongo
 
         public async Task<int> Count(Expression<Func<T, bool>> predicate)
         {
-            var database = GetConnection();
-            var collectionName = GetCollectionName();
-            var response = await database.GetCollection<T>(collectionName).Find(predicate).ToListAsync();
-
+            var response = await _collection.Find(predicate).ToListAsync();
             return response.Count;
         }
 
