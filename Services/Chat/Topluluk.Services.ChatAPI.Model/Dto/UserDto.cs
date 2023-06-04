11365c016eb5cd743bb6b6f92499879450e5a5ee
İsmail Ertuğlu <ismail.ertuglu@quanticst.com>
2023-06-04 13:23:28 +0300

    .

---
+++ Services/Chat/Topluluk.Services.ChatAPI.Model/Dto/UserDto.cs
@@ -0,0 +1,10 @@
+namespace Topluluk.Services.ChatAPI.Model.Dto;
+
+public class UserDto
+{
+    public string Id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string ProfileImage { get; set; }
+    public string Gender { get; set; }
+}
